121952d9d9d659d695e5b0a0eff6db658e29e99b
Lee Su Hyeon <dltngus12323@gmail.com>
2023-09-26 02:03:12 +0900

    과제 완성

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -1,51 +1,55 @@
 from pico2d import *
 
-
-open_canvas()
-grass = load_image('grass.png')
-character = load_image('animation_sheet.png')
-
+TUK_WIDTH, TUK_HEIGHT = 1280, 1024
+open_canvas(TUK_WIDTH, TUK_HEIGHT)
+tuk_ground = load_image('TUK_GROUND.png')
+character = load_image('sprite_sheet.png')
 
 def handle_events():
-    global running, dir
-
-    # fill here
-
-    global x
+    global running, dir_x, dir_y
 
     events = get_events()
+
     for event in events:
         if event.type == SDL_QUIT:
             running = False
-        # fill here
         elif event.type == SDL_KEYDOWN:
             if event.key == SDLK_RIGHT:
-                dir += 1
+                dir_x += 1
             elif event.key == SDLK_LEFT:
-                dir -= 1
+                dir_x -= 1
+            elif event.key == SDLK_DOWN:
+                dir_y -= 1
+            elif event.key == SDLK_UP:
+                dir_y += 1
             elif event.key == SDLK_ESCAPE:
                 running = False
         elif event.type == SDL_KEYUP:
-            if event.type == SDLK_RIGHT:
-                dir -= 1
-            elif event.type == SDLK_LEFT:
-                dir += 1
+            if event.key == SDLK_RIGHT or event.key == SDLK_LEFT:
+                dir_x = 0
+            elif event.key == SDLK_DOWN or event.key == SDLK_UP:
+                dir_y = 0
 
 running = True
-x = 800 // 2
 frame = 0
-dir = 0
+x, y = TUK_WIDTH // 2, TUK_HEIGHT // 2
 
-# fill here
+dir_x = 0
+dir_y = 0
+
 while running:
+
     clear_canvas()
-    grass.draw(400, 30)
-    character.clip_draw(frame * 100, 0, 100, 100, x, 90)
+    tuk_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
+    character.clip_draw(frame * 128, 128 * 15, 128, 128, x, y, 150, 150)
     update_canvas()
     handle_events()
     frame = (frame + 1) % 8
-    x += dir * 5
-    delay(0.05)
+
+    if 64 < x + dir_x * 10 < TUK_WIDTH - 64 and 0 < y + dir_y * 10 < TUK_HEIGHT - 64:
+        x += dir_x * 10
+        y += dir_y * 10
+
+    delay(0.03)
 
 close_canvas()
-
